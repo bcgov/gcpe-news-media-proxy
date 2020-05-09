dcfd8773aa69cc5461cd1cfa1ac87f788c16a8f6
André Lashley <andre.lashley@gmail.com>
2020-05-08 21:05:30 -0700

    Update Startup.cs

--- Gov.News.Media.Website/Startup.cs
+++ Gov.News.Media.Website/Startup.cs
@@ -66,7 +66,7 @@ namespace Gov.News.Media.Website
             });
             app.UseFileServer(new FileServerOptions()
             {
-                RequestPath = new PathString("translations"),
+                RequestPath = new PathString("/translations"),
                 EnableDirectoryBrowsing = false
             });
 
